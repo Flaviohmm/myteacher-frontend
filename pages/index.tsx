3bd66a9f888a2520e83af858fedd00b199cfb1a2
Flavio Macedo <fhenrique609@gmail.com>
2022-10-07 13:46:38 -0300

    Criar listagem de professores

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,44 @@
+import { Box } from '@mui/material'
 import type { NextPage } from 'next'
+import { Professor } from '../src/@types/professor'
 import Cabecalho from '../src/components/Cabecalho/Cabecalho'
+import Lista from '../src/components/Lista/Lista'
 
 const Home: NextPage = () => {
+  const professores: Professor[] = [
+    {
+      id: 1,
+      nome: "Professor 1",
+      foto: "https://github.com/flaviohmm.png",
+      descricao: "Descrição do professor 1",
+      valor_hora: 100
+    },
+    {
+      id: 2,
+      nome: "Professor 2",
+      foto: "https://github.com/flaviohmm.png",
+      descricao: "Descrição do professor 2",
+      valor_hora: 200
+    },
+    {
+      id: 3,
+      nome: "Professor 3",
+      foto: "https://github.com/flaviohmm.png",
+      descricao: "Descrição do professor 3",
+      valor_hora: 300
+    },
+    {
+      id: 4,
+      nome: "Professor 4",
+      foto: "https://github.com/flaviohmm.png",
+      descricao: "Descrição do professor 4",
+      valor_hora: 400
+    }
+  ]
   return (
-    <>
-      <div>Conteudo</div>
-    </>
+    <Box sx={{ backgroundColor: 'secondary.main' }}>
+      <Lista professores={professores}></Lista>
+    </Box>
   )
 }
 
